6e72002eafb78176440dd902ca1b8206468fd43b
Rochi <loker9898@gmail.com>
2024-04-26 15:16:22 +0300

    feature test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,9 +7,15 @@ from config import ROOT_DIR
 class FuncsTest(unittest.TestCase):
     def test_read(self):
         self.assertEqual(src.utils.read_operations(os.path.join(ROOT_DIR, 'tests', 'data.json')),
-                         [{'1': 'a'}, {'2': 'b'}, {'3': 'c'}])
+                         [{"1": "a", "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041"},
+                          {"2": "b", "state": "CANCELED", "date": "2018-08-26T10:50:58.294041"},
+                          {"3": "c", "state": "EXECUTED", "date": "2017-08-26T10:50:58.294041"}
+                          ])
+
+    def test_get_last_operations(self):
+        self.assertEqual(src.utils.get_last_operations(1),
+                         [{"1": "a", "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041"}])
 
 
 if __name__ == '__main__':
     unittest.main()
-
